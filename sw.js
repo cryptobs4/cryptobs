ee206bf423bb12343429c78e195c89e576ef46dc
cryptobs4 <learntanin@gmail.com>
2026-03-05 12:28:45 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,29 @@
-const CACHE = "cryptobs-pwa-v2";
+const CACHE = "cryptobs-pwa-v1";
 
 const ASSETS = [
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "https://cryptobs4.github.io/cryptobs/",
+  "https://cryptobs4.github.io/cryptobs/index.html",
+
+  "https://cryptobs4.github.io/cryptobs/manifest.json",
+  "https://cryptobs/icon-192.png",
+  "https://cryptobs/icon-512.png"
 ];
 
+// Install: cache core assets
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
 });
 
+// Activate: remove old caches
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    )
   );
 });
 
+// Fetch: cache-first, then network
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
